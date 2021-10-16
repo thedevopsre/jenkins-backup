a014f670203fbe2e293e1a36478c558d3493e933
vndark <vahenyan77@gmail.com>
2021-10-16 14:15:21 +0400

    Changed urls to variables, added help

--- jenkins-backup-s3.sh
+++ jenkins-backup-s3.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-# 0 7 * * * /root/jenkins-backup-s3.sh - FOR CRONTAB JOB
+# You can setup script for cronjob
+# 0 7 * * * /root/jenkins-backup-s3.sh
 
 #SCRIPT backup and PUSH TO AWS S3
 
 #Set backup time and Country
-backup_time=$(TZ=":USA/Central" date "+%Y-%m-%d**%H:%M:%S")
-path_backup="/root/jenkins_backup/permanent"
+backup_time=$(TZ=":USA/Central" date "+%Y-%m-%d**%H:%M:%S") # Or you can setup your time zone
+path_backup="/root/jenkins_backup/permanent" # For example I backup my jenkins to /root/
+s3bcuketurl="s3://examplebucket/bucket/filename.tar.gz" # Rememmber !!! set file name example` backupjenkins.tar.gz
 
 cd $path_backup
-for var in $( find $path_backup -ctime  -3 \( -name "*.tar.gz" -o  -name "backup_*.tar.gz" \)  -print ); do aws s3 cp $var s3://url-to-s3/backup_latest.tar.gz ;done
-#Log to file backup success or not
+
+for var in $( find $path_backup -ctime  -3 \( -name "*.tar.gz" -o  -name "backup_*.tar.gz" \)  -print ); do aws s3 cp $var $s3bcuketurl ;done
+# Log to file backup success or not
 if [[ -f $var ]]; then
-	echo "Success backup to S3  | USA-Central  - $backup_time" >> $path_backup/backup_logs.log
+	echo "Success backup to S3  | Time  - $backup_time" >> $path_backup/backup_logs.log
  elif [[ ! -f $var ]]; then
- 	echo "Failed backup files are not exists in directory | USA-Central - $backup_time" >> $path_backup/backup_errors.log
+ 	echo "Failed backup files are not exists in directory | Time- $backup_time" >> $path_backup/backup_errors.log
 fi
-#Clean after pushing zip to s3
+
+# Clean after pushing zip to s3
 cd $path_backup
 for tar in $( find $path_backup -ctime  -3 \( -name "*.tar.gz" -o  -name "backup_*.tar.gz" \)  -print ); do rm $tar ;done
 if [[ ! -f $tar ]]; then
-	echo "After backup tar.gz files are deleted  | USA-Central - $backup_time" >> $path_backup/backup_logs.log
+	echo "After backup tar.gz files are deleted  | TIme - $backup_time" >> $path_backup/backup_logs.log
 fi
 
 exit 0
